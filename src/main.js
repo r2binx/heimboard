2cd1a2e6144b7d15bb479f91e4f8d23c4769e164
Robin Härle <robin@blckct.io>
2021-12-17 19:54:30 +0100

    🔧️ Update PWA config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
 import App from './App.vue'
+
+const intervalMS = 60 * 60 * 1000
+
+const updateServiceWorker = useRegisterSW({
+    onRegistered(r) {
+        r && setInterval(() => {
+            r.update()
+        }, intervalMS)
+    }
+})
 
 const app = createApp(App)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,36 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
-  VitePWA()]
+  VitePWA({
+    mode: "development",
+    base: "/",
+    srcDir: "src",
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'HEIMBOARD',
+      short_name: 'HEIM',
+      description: 'Dashboard for the blckct.io server',
+      theme_color: '#ffffff',
+      display: 'standalone',
+      start_url: "/",
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ],
+    }
+  })]
 })
