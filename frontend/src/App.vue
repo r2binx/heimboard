d106e8f704d70379c2791b1010719ff3ec216ff1
Robin Härle <robin@blckct.io>
2021-12-22 12:54:00 +0100

    🎨 import svg

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import RefreshOutlined from "./assets/RefreshOutlined.svg"
 import { provide, ref, watch } from "vue";
 import {
   NConfigProvider,
@@ -57,16 +58,7 @@ function changeTheme(newTheme) {
           >
             <template #icon>
               <n-icon>
-                <!-- TODO properly import instead of shitty inline svg -->
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M17.65 6.35A7.958 7.958 0 0 0 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
-                  />
-                </svg>
+                <RefreshOutlined />
               </n-icon>
             </template>
           </n-button>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -35,5 +36,6 @@ export default defineConfig({
         }
       ],
     }
-  })]
+  }),
+  svgLoader()]
 })
