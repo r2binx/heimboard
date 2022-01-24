bad30db2b42cfaad4133e5b1c2d3b1ea04c21df8
Robin Härle <robin@blckct.io>
2022-01-24 16:58:04 +0100

    :sparkles: enable new reactivity transform
    see https://github.com/vuejs/rfcs/discussions/369

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,37 +5,39 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  VitePWA({
-    registerType: 'autoUpdate',
-    base: "/",
-    srcDir: "src",
-    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-    manifest: {
-      name: 'HEIMBOARD',
-      short_name: 'HEIM',
-      description: 'Dashboard for the blckct.io server',
-      display: 'standalone',
-      start_url: "/",
-      icons: [
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        }
-      ],
-    }
-  }),
-  svgLoader()]
+    plugins: [vue({
+        reactivityTransform: true
+    }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            base: "/",
+            srcDir: "src",
+            includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'HEIMBOARD',
+                short_name: 'HEIM',
+                description: 'Dashboard for the blckct.io server',
+                display: 'standalone',
+                start_url: "/",
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    }
+                ],
+            }
+        }),
+        svgLoader()]
 })
